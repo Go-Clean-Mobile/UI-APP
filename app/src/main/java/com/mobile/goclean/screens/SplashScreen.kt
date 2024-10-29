ea2e0864b580c6dce4ce8b00a7bca90352f7dee7
Regi Pratama <regipratama@Fayakun.local>
2024-10-29 18:38:21 +0700

    Gilang UY

--- app/src/main/java/com/mobile/goclean/screens/SplashScreen.kt
+++ app/src/main/java/com/mobile/goclean/screens/SplashScreen.kt
@@ -6,16 +6,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.mobile.goclean.R
 import com.mobile.goclean.theme.BackgroundGradient
 import com.mobile.goclean.theme.OnPrimaryColor
+import com.mobile.goclean.theme.ooredo
 import kotlinx.coroutines.delay
 
 @Composable
@@ -40,6 +43,14 @@ fun SplashScreen(navController: NavController) {
             contentDescription = "Login",
             tint = OnPrimaryColor,
             modifier = Modifier.size(150.dp),
+        )
+        Text(
+            "GoClean",
+            style =
+                TextStyle(
+                    fontFamily = ooredo,
+                    color = OnPrimaryColor,
+                ),
         )
     }
 }
